b4775b9718b1675a6490cfb3b3ae84431b8f9ef3
Solene Baudic <solene.baudic@gmail.com>
2024-07-03 14:35:36 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "0611224455", category:"français"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "0611224455", category:"french"}
+indian_food = {name: "Indian", address: "Paris", phone_number: "0611224455", category:"indian"}
+chinese_food = {name: "Chinese", address: "Lille", phone_number: "0611224455", category:"chinese"}
+corean_food = {name: "Corean", address: "Bordeaux", phone_number: "0611224455", category:"corean"}
+
+[dishoom, pizza_east, indian_food, chinese_food, corean_food].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
